21c993277b4b3fd0607602069fd1ff11e2d3cf7f
rachitdBeast <81420042+rachitdBeast@users.noreply.github.com>
2021-11-24 11:09:18 +0530

    Add files via upload

---
+++ 3_Implementation/diary.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include "afro.h"
+
+FILE *fp;
+int main()
+{
+   welcome_message();
+    char press;
+    int num;
+First:
+    printf("\n\n---- Press ----- operation ------\n\n");
+    printf("     '+' ------ add new note     \n");
+    printf("     'a' ------ append new note     \n");
+    printf("     'v' ------ View Previous     \n");
+    printf("     'e' ------ Exit Program     \n\n");
+    printf("-------------------------------\n\n");
+
+while(1)
+{
+    scanf("%c",&press);
+
+    if (press == '+'){
+        insert();
+        printf("Press '1' for continue\n'0' for Exit\n");
+        scanf("%d", &num);
+        if (num == 1){
+            goto First;
+        }
+        else{
+            break;
+        }
+    }
+    else if (press == 'a'){
+        append();
+        printf("Press '1' for continue\n'0' for Exit\n");
+        scanf("%d", &num);
+        if (num == 1){
+            goto First;
+        }
+        else{
+            break;
+        }
+    }
+    else if (press == 'v'){
+        view();
+        printf("Press '1' for continue\n'0' for Exit\n");
+        scanf("%d", &num);
+        if (num == 1){
+            goto First;
+        }
+        else{
+            break;
+        }
+    }
+    else if (press == 'e'){
+        return 0;
+    }
+    else{
+        printf("Please press correct key.\n");
+    }
+}
+    return 0;
+}
+
+
+
+
